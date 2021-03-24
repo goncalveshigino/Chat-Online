f31e1d381b7ae89b0853fddd8ed4b3e8ba288ef3
adilsonchameia <adilsonkchameia@outlook.com>
2021-03-24 22:04:35 +0100

    Campo de digitacao

---
+++ lib/screens/home/chat_screen.dart
@@ -0,0 +1,21 @@
+import 'package:chatoline/text_composer.dart';
+import 'package:flutter/material.dart';
+
+
+class ChatScreen extends StatefulWidget {
+  @override
+  _ChatScreenState createState() => _ChatScreenState();
+}
+
+class _ChatScreenState extends State<ChatScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+       appBar: AppBar(
+         title: Text('Hello'),
+         centerTitle: true,
+       ),
+       body: TextComposer(),
+    );
+  }
+}
