940c43ed31c84a29a8331f1ed8b6f15f1ca363bb
adilsonchameia <adilsonkchameia@outlook.com>
2021-03-24 21:12:41 +0100

    Lendo dados do banco

--- lib/main.dart
+++ lib/main.dart
@@ -1,34 +1,36 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
-
- 
 void main() async {
-  
   runApp(MyApp());
 
-Firestore firestore = Firestore.instance;
-
+  Firestore firestore = Firestore.instance;
 
 //  QuerySnapshot snapshot = await Firestore.instance.collection('message').getDocuments();
 //   snapshot.documents.forEach((d) {
 //     d.reference.updateData({'lido': false});
 //    });
 
-firestore.collection('message').snapshots().listen((dado) {
-  dado.documents.forEach((d) { print(d.data);
-   });
- });
-   
-} 
- 
+// firestore.collection('message').snapshots().listen((dado) {
+//   dado.documents.forEach((d) { print(d.data);
+//    });
+//  });
+
+  firestore
+      .collection('message')
+      .document('WxcVOCZ29K3udjzjb837')
+      .snapshots()
+      .listen((dado) {
+    print(dado.data);
+  });
+}
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: true,
-      title: 'Material App',
-      home: Scaffold()
-    );
+        debugShowCheckedModeBanner: true,
+        title: 'Material App',
+        home: Scaffold());
   }
 }
